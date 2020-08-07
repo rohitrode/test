dd8517734dc9b19c2aa141dfb2412f15ce139fca
rohit <rohitrode1987@gmail.com>
2020-08-07 18:24:10 +0530

    Opps-247Walacc

--- first.py
+++ first.py
@@ -4,6 +4,6 @@ print("Hello World")
 hungry = input("Are you Hungry ?")
 
 if hungry:
-    print("Eat Samosa or vada pav")
+    print("Eat Samosa or vada pav or pav bhaji")
 else:
     print("Do Your Task")
